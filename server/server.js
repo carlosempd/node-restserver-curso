d176eb97a32250c74a4cdb67ba8ade2e477d5df6
Carlos <carlosempd@gmail.com>
2019-08-01 16:13:25 -0400

    RESTserver - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -11,45 +14,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-//get -> READ
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
+app.use(require('./routes/usuario'));
+
+
+
+mongoose.connect(process.env.URLDB, (err, resp) => {
+    if (err) throw new err;
+
+    console.log("Base de datos ONLINE");
 });
 
-//post -> CREAR
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-
-});
-
-//put -> ACTUALIZAR
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-//delete -> DELETE
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-});
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando el puerto: ", process.env.PORT);
-})
+});
